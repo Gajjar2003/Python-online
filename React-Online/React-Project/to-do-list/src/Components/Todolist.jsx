c216b5d7a2c13c29928c8d85485fc6d50556f720
JENIL1108 <jenilgajjar1108@gmail.com>
2026-07-22 17:33:59 +0530

    all

---
+++ React-Online/React-Project/to-do-list/src/Components/Todolist.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+function Todolist() {
+  return (
+    <div>
+      <h1>Wel Come To-Do-LIst</h1>
+      <h2>To-Do-App</h2>
+      <br />
+       
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <input
+              type="text"
+              placeholder="Enter Todo Here..."
+              style={{ backgroundColor: "white" }}
+            />
+          </div>
+          <div class="col">
+            <input type="date" />
+          </div>
+          <div class="col">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    <br />
+       <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            PC
+          </div>
+          <div class="col">
+          22/06/2026
+          </div>
+          <div class="col">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+        <br />
+       <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            Laptop
+          </div>
+          <div class="col">
+            11/08/2025
+          </div>
+          <div class="col">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Todolist;
