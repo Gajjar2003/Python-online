10443367a89d49f781799880a44c763c94ff3922
JENIL1108 <jenilgajjar1108@gmail.com>
2026-08-18 17:39:41 +0530

    all

--- Python-Project/booking/admin.py
+++ Python-Project/booking/admin.py
@@ -1,3 +1,54 @@
-from django.contrib import admin
+from django import forms
+from booking.models import Booking
 
-# Register your models here.
+
+class BookingForm(forms.ModelForm):
+
+    class Meta:
+        model = Booking
+
+        fields = [
+            "username",
+            "email",
+            "mobile",
+            "slot_time",
+            "duration",
+        ]
+
+        widgets = {
+
+            "username": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter username"
+                }
+            ),
+
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter email"
+                }
+            ),
+
+            "mobile": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter mobile number"
+                }
+            ),
+
+            "slot_time": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "readonly": True
+                }
+            ),
+
+            "duration": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                    "readonly": True
+                }
+            ),
+        }
--- Python-Project/booking/models.py
+++ Python-Project/booking/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+
+    # Logged-in user
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    # User information
+    username = models.CharField(
+        max_length=150
+    )
+
+    email = models.EmailField()
+
+    mobile = models.CharField(
+        max_length=15
+    )
+
+    # Cricket slot
+    slot_time = models.CharField(
+        max_length=20
+    )
+
+    # Booking duration
+    duration = models.IntegerField()
+
+    # Total price
+    price = models.IntegerField()
+
+    # Booking created date/time
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+
+        return (
+            f"{self.username} - "
+            f"{self.slot_time} - "
+            f"{self.duration} Hour"
+        )
